eb5f6ec0d5ea7931b78f2fd91f42d1a724b78df8
Andrew Fresh <andrew@afresh1.com>
2014-01-01 17:57:33 -0800

    Rework XDeadly::PostMod to not require disk access
    Just so I can test it.  Should keep it working better I expect.

--- lib/XDeadly/PostMod.pm
+++ lib/XDeadly/PostMod.pm
@@ -18,58 +18,65 @@ use Mojo::Base -base;
 our $VERSION = '0.01';
 
 use Mojo::Asset::File;
+use List::Util qw( sum );
 
 has 'post';
 
 has _points => sub {
     my ($self) = @_;
-    return unless $self->post;
 
-    my %mod = (
-        sum   => 0,
-        count => 0,
-        votes => {},
-    );
-    return \%mod unless -e $self->path;
+    my %mod = ();
+    return \%mod unless $self->path and -e $self->path;
 
     my $file = Mojo::Asset::File->new( path => $self->path );
 
     foreach ( split /\n/, $file->slurp ) {
         my ( $who, $what ) = split /\s+/;
 
-        $mod{votes}{$who} = $what;
-        next unless $what;
-
-        $mod{count}++;
-        $mod{sum} += $what;
+        $mod{$who} = $what;
     }
 
     return \%mod
 };
 
-sub path { return shift->post->path . '.mod' }
+sub path { 
+    my $post = shift->post;
+    return unless $post;
+    return $post->path . '.mod';
+}
 
 sub vote {
     my ($self, $who, $what) = @_;
 
-    my $mod = delete $self->{_points};
-    $mod->{votes}->{$who} = $what;
+    my $mod = $self->_points->{$who} = $what;
+
+    $self->_save if $self->path;
+
+    return $self;
+}
+
+sub _save {
+    my ($self) = @_;
 
     open my $fh, '>', $self->path or die "Couldn't update modfile: $!";
-    foreach my $who (keys %{ $mod->{votes} }) {
-        print $fh "$who $mod->{votes}->{$who}\n";
-    }
+
+    my $mod = delete $self->{_points};
+    print $fh $_ for map { "$_ $mod->{$_}\n" } sort keys %{ $mod };
+
     close $fh;
 
     return $self;
 }
 
-sub score { shift->_points->{sum} }
-sub votes { shift->_points->{count} }
-sub has_voted { 
-    my ($self, $who) = @_;
-    my $votes = $self->_points->{votes};
-    return exists $votes->{$who};
+sub score { sum( values %{ shift->_points }, 0 ) }
+
+sub votes {
+    scalar grep {$_} values %{ shift->_points };
+}
+
+sub has_voted {
+    my ( $self, $who ) = @_;
+    return exists $self->_points->{$who};
 }
 
 1;
