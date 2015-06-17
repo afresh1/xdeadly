1133e3f7f4af02c0b8cd05907818503fcfbda03e
Tim Howe <timh@dirtymonday.net>
2015-06-16 19:45:46 -0700

    next iteration of errata with cleanup and minor improvements

---
+++ script/errata_scraper.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/env perl
+########################################################################
+# Copyright (c) 2011 Andrew Fresh <andrew@afresh1.com>
+# Copyright (c) 2015 Tim Howe <timh@dirtymonday.net>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+########################################################################
+use strict;
+use warnings;
+
+use feature 'postderef';
+no warnings 'experimental::postderef';
+
+use Mojo::UserAgent;
+use Mojo::JSON qw(encode_json);
+
+my $base_uri = 'http://www.openbsd.org/';
+
+my $ua = Mojo::UserAgent->new;
+
+my $ls = $ua->get( $base_uri . 'errata.html' )->res->dom('a[href^="errata"]');
+
+my $errata = [];
+
+foreach my $l ( @{$ls}[ -2, -1 ] ) {
+    my $vertitle = 'Errata for OpenBSD ' . $l->text;
+    my $entries = [];
+    foreach my $e (
+        reverse $ua->get( $base_uri . $l->attr->{'href'}
+                         )->res->dom('body > ul > li')->@*
+                  )
+    {
+        my $patch = $e->at('a[href$=".patch.sig"]')->attr('href') if
+                        defined $e->at('a[href$=".patch.sig"]');
+        my $title = $e->at('font > strong')->text;
+        my $arch  = $e->at('i')->text;
+
+        ## FIXME! This needs to be something that might work better. ##
+        ## I recommend improving the structure of the errata pages. --TimH ##
+        # For description I just use everything after the first <br> tag.
+        # That probably will only work until someone starts typing it
+        # different.
+        # This leaves the HTML intact for the description.
+        my $descr = $e->to_string;
+        $descr =~ s/.*?<br>//s;
+        ##
+
+        $descr =~ s/\A\s+//;
+        $descr =~ s/\s+/ /gs;
+        $descr =~ s/\s(\.(?:\s|$))/$1/gs;
+        $descr =~ s/\.+$/./gs;
+
+        push $entries->@*, {
+            'title' => $title,
+            'arch'  => $arch,
+            'patch' => defined $patch ? $patch : '',
+            'descr' => $descr, };
+    }
+
+    push $errata->@*, { $vertitle => $entries };
+}
+
+print encode_json($errata);
+
